32e7c1a71652c722d3ab99e2da250526f21957a8
Golfier-Maxime <maxime.golfier@outlook.fr>
2023-01-24 21:40:25 +0100

    Commit init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import headerVue from "@/components/header.vue"
+</script>
+
+<template>
+
+  <div :class="isDark ? 'dark' : ''"> <!--si isDark = true, Dark mode | si isDark = false, Light mode-->
+    <headerVue></headerVue>
+
+    <body class="bg-[#eee] dark:bg-[#333] duration-700">
+      <!-- Rajouter une transition duration pour une transition de DarkMode à LightMode + douce -->
+
+      <div class="absolute gap-3 flex top-[30px] left-[30px] items-center">
+        <p class="relative text-lg dark:text-white duration-700">Dark Mode</p>
+        <!-- Début Bouton Switch -->
+        <label
+          class="switch relative dark:border-transparent dark:hover:border-white border-2 rounded-full duration-700">
+          <input type="checkbox" checked @click="isDark = !isDark">
+          <span class="slider round bg-white before:bg-[#222] dark:before:bg-white"></span>
+        </label>
+        <!-- Fin Bouton Switch -->
+      </div>
+
+    </body>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      isDark: true,
+    };
+  },
+  components: {
+
+  },
+};
+</script>
---
+++ src/components/header.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-white dark:bg-black">
+        <p class="dark:text-white duration-700">Kakuhen</p>
+    </div>
+</template>
+
+<style>
+
+</style>
