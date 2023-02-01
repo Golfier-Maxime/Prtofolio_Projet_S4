ad1c921646bc7cdfa8fb6a59d3c8a62478e24343
Golfier-Maxime <maxime.golfier@outlook.fr>
2023-02-01 21:30:50 +0100

    fix

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -9,9 +9,9 @@
         </div>
         <div class="">
             <p class="dark:text-white duration-700">Kakuhen</p>
-            <img src="public\images\Linkedin.svg" alt="">
-            <img src="public\images\Twitter.svg" alt="">
-            <img src="public\images\Github.svg" alt="">
+            <img src="images\Linkedin.svg" alt="">
+            <img src="images\Twitter.svg" alt="">
+            <img src="images\Github.svg" alt="">
         </div>
     </footer>
 </template>
