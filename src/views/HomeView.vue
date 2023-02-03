82d010348d2bb0227ff37334e533e9f36ca876fe
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-02-03 12:59:49 +0100

    debut home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,13 @@
 </script>
 
 <template>
-    <h1>MAXIME GOLFIER</h1>
+    <div>
+        <h1 class="mt-4 text-center titre tracking-widest text-[60px]  dark:text-white duration-700">MAXIME GOLFIER</h1>
+        <div>
+            <img src="" alt="">
+            <div>
+                <h2>Etudiant Web devellopement</h2>
+            </div>
+        </div>
+    </div>
 </template>
