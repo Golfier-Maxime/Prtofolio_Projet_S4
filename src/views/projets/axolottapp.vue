c8c17113be333d71d06e2af98014b7313837e812
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-03-18 14:54:49 +0100

    axolottapp pc

--- src/views/projets/axolottapp.vue
+++ src/views/projets/axolottapp.vue
@@ -8,9 +8,69 @@
             class="mt-4 lg:mt-12 text-center titre tracking-[6px] text-[60px] lg:text-[76px] 2xl:text-[100px]  dark:text-white duration-700">
             AXOLOTT</h1>
         <div class="ml-4 mr-4 lg:mx-20 ">
-            <div class="flex justify-center lg:mt-8 lg:gap-16">
+            <div class="flex justify-center lg:mt-16 lg:gap-16">
                 <img src="/images/axolottapp_site.jpg" alt="" class="lg:w-[700px]">
                 <img src="/images/axolottapp_mobile.jpg" alt="" class="lg:w-[330px]">
+            </div>
+            <div>
+                <h2
+                    class="mt-16 text-lg lg:text-[20px] 2xl:text-[28px] font-bold tracking-wider dark:text-white duration-700">
+                    Le projet</h2>
+                <div class="bar-title h-1 mt-2 2xl:w-[150px] lg:w-[110px]"></div>
+                <p class="lg:mt-16 lg:text-[18px]">Axolott est un projet universitaire de fin de première année en groupe.
+                    Le
+                    but était de
+                    créer un
+                    site web qui renouvelle la routine avec des missions qui vous offre des points.
+                    Avec, vous pouvez acheter tout type d'accessoire pour modifier votre Axolotl de companie.
+
+                    La contrainte était de réaliser ce projet avec Vue3js et tailwind. ce qui fu d'une grande nouveauté.
+                    En plus de devoir créer une identité visuel du produit.</p>
+            </div>
+            <div class="flex justify-center lg:mt-20 lg:gap-16">
+                <div>
+                    <h3 class="lg:mt-16 lg:text-[20px] 2xl:text-[28px]">L'Axolotl</h3>
+                    <div class="bar-title h-1 mt-2 2xl:w-[140px] lg:w-[120px]"></div>
+                    <p class="lg:mt-8 lg:text-[18px]">L'axolotl est l'animal parfait pour nous représenter.
+                        Il y a une multitude d'axolotl différent comme le nombre de quète disponible.
+                        En plus il est parfait pour être modifiable à volonté, de toute les couleurs habillez le
+                        comme vous le voulez.
+                    </p>
+                </div>
+                <img src="/images/axolott_mascot.png" alt="" class="lg:w-[600px] lg:h-[270px] lg:mt-8 lg:mr-8">
+            </div>
+            <div class="flex justify-center lg:mt-20 lg:gap-16">
+                <div>
+                    <h3 class="lg:mt-16 lg:text-[20px] 2xl:text-[28px]">Design</h3>
+                    <div class="bar-title h-1 mt-2 2xl:w-[100px] lg:w-[80px]"></div>
+                    <p class="lg:mt-8 lg:text-[18px]">Ces couleurs représente l'esprit Axolott. Les deux couleurs
+                        principales que sont les
+                        violets,
+                        représente
+                        le coté jeux et ludique, nous prenons comme exemple le site Twitch. Quant aux deux couleurs
+                        toniques, celle-ci
+                        sont présentent pour souligner les call-to-action ou les éléments importants du site.
+                    </p>
+                </div>
+                <img src="/images/axolott_color.png" alt="" class="lg:w-[700px]">
+            </div>
+            <div class="flex justify-center lg:mt-20 g:gap-16">
+                <div>
+                    <h3 class="lg:mt-16 lg:text-[20px] 2xl:text-[28px]">Typographie</h3>
+                    <div class="bar-title h-1 mt-2 2xl:w-[170px] lg:w-[140px]"></div>
+                    <p class="lg:mt-8 lg:text-[18px]">Ces couleurs représente l'esprit Axolott. Les deux couleurs
+                        principales que sont les
+                        violets,
+                        représente
+                        le coté jeux et ludique, nous prenons comme exemple le site Twitch. Quant aux deux couleurs
+                        toniques, celle-ci
+                        sont présentent pour souligner les call-to-action ou les éléments importants du site.
+                    </p>
+                </div>
+                <img src="/images/axolott_font.png" alt="" class="lg:w-[700px] rounded-lg">
+            </div>
+            <div class="flex justify-center lg:lg:mt-20">
+                <img src="/images/axolott_bye.png" alt="" class="lg:w-[400px]">
             </div>
         </div>
     </div>
